7ebc0d2b17787f601781c1995411dc1956cda966
unknown <celine007@gmail.com>
2020-09-21 09:59:36 -0700

    Updates

---
+++ precache-manifest.ddebc4723e6df7f06861d9b1635db0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e468ebbc83113191f0687c762543e03",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "bd39937777c8f6314cb9",
+    "url": "/pomodoro-clock/static/css/main.db1e2695.chunk.css"
+  },
+  {
+    "revision": "c9cf4c27b7211e12371b",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd39937777c8f6314cb9",
+    "url": "/pomodoro-clock/static/js/main.37f72fbc.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
